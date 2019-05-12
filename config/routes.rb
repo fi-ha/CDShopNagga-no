55da2dd6409243a390c8dbd0a41ed8d76dc35f11
ama <takuyaamano@amamo.local>
2019-05-12 12:31:46 +0900

    [create]rails g controller genres

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'genres/index'
+  get 'genres/new'
+  get 'genres/edit'
+  get 'genres/create'
+  get 'genres/update'
+  get 'genres/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
