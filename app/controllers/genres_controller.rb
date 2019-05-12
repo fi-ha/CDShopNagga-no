14b2b80ed2003e5ce8a79312eb89c1416f373238
ama <takuyaamano@amamo.local>
2019-05-12 13:12:46 +0900

    [update]genreコントローラのprivate部分を追加

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -16,4 +16,10 @@ class GenresController < ApplicationController
 
   def destroy
   end
+
+  private
+  # httpリクエストのPOSTの時に内部に流れてきたgenreキーを持つパラメータをコントローラ内部に受け取ることを許可する
+    def genre_params
+      params.require(:genre).permit(:genre_name)
+    end
 end
