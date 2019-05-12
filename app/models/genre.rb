55e61daa525f0fac3ef329691761e578a96c73ce
ama <takuyaamano@amamo.local>
2019-05-12 13:59:51 +0900

    [update]item_genresに対してのアソシエーション追加

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,4 +1,7 @@
 class Genre < ApplicationRecord
+  # アソシエーション設定
+  has_many :item_genres #itemsとgenresの中間テーブルへのアソシエーション
+
   # フォームを保存する際に以下のバリデーションを設定している
   validates :name,
     presence: true, # 空では無い
