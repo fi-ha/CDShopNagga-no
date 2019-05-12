ec57304dc628e7e81ae2226e776854e95401cb98
amamo <ama@amamonoMacBook-Pro.local>
2019-05-12 21:49:59 +0900

    [create][update]ship_to_anotherのルート設定ビュー、コントローラーを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
-  get 'item_carts/create'
-  get 'item_carts/update'
   get 'carts/:id/edit',                 to: 'carts#edit', as: 'cart_edit'
   get 'carts/:id/ship_to_another/edit', to: 'catrs#edit', as: 'ship_to_another_edit'
   get 'carts/:id/payment/edit',         to: 'catrs#edit', as: 'payment_edit'
   get 'carts/:id/confirm/edit',         to: 'catrs#edit', as: 'confirm_edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :genres,     only: [:index, :new, :edit, :create, :update, :destroy]
-  resources :carts,      only: [:index, :new, :create, :update]
-  resources :item_carts, only: [:create, :update]
+  resources :genres,           only: [:index, :new, :edit, :create, :update, :destroy]
+  resources :carts,            only: [:index, :new, :create, :update]
+  resources :item_carts,       only: [:create, :update]
+  resources :ship_to_anothers, only: [:create, :new, :update]
 
 end
