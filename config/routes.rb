f48687ed54a624a04c436e110e5c8e2983742385
amamo <ama@amamonoMacBook-Pro.local>
2019-05-12 20:41:55 +0900

    [create][fix]carts_finishのviewを追加とルートの誤字修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'carts/:id/edit',                 to: 'cats#edit', as: 'cart_edit'
-  get 'carts/:id/ship_to_another/edit', to: 'cats#edit', as: 'ship_to_another_edit'
-  get 'carts/:id/payment/edit',         to: 'cats#edit', as: 'payment_edit'
-  get 'carts/:id/confirm/edit',         to: 'cats#edit', as: 'confirm_edit'
+  get 'carts/:id/edit',                 to: 'carts#edit', as: 'cart_edit'
+  get 'carts/:id/ship_to_another/edit', to: 'catrs#edit', as: 'ship_to_another_edit'
+  get 'carts/:id/payment/edit',         to: 'catrs#edit', as: 'payment_edit'
+  get 'carts/:id/confirm/edit',         to: 'catrs#edit', as: 'confirm_edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :genres, only: [:index, :new, :edit, :create, :update, :destroy]
