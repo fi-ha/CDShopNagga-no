c01ed2d3931c0ed8e477d5b8aa8f31391ecfe7ee
ama <takuyaamano@amamo.local>
2019-05-12 16:53:59 +0900

    [update][create]カート内のビューとrouteを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  get 'carts/index'
-  get 'carts/new'
-  get 'carts/edit'
-  get 'carts/create'
-  get 'carts/update'
+  get 'carts/:id/edit',                 to: 'cats#edit', as: 'cart_edit'
+  get 'carts/:id/ship_to_another/edit', to: 'cats#edit', as: 'ship_to_another_edit'
+  get 'carts/:id/payment/edit',         to: 'cats#edit', as: 'payment_edit'
+  get 'carts/:id/confirm/edit',         to: 'cats#edit', as: 'confirm_edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :genres, only: [:index, :new, :edit, :create, :update, :destroy]
-  
+  resources :carts,  only: [:index, :new, :create, :update]
+
 end
