dfe582f038db41d2ecd41038934d40496395432d
sato <>
2019-05-13 18:28:50 +0900

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'carts/:id/edit',                 to: 'carts#edit', as: 'cart_edit'
   get 'carts/:id/ship_to_another/edit', to: 'catrs#edit', as: 'ship_to_another_edit'
   get 'carts/:id/payment/edit',         to: 'catrs#edit', as: 'payment_edit'
   get 'carts/:id/confirm/edit',         to: 'catrs#edit', as: 'confirm_edit'
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :items do
     resource :item_carts,  only: [:create, :update]
