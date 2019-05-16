44b037aa8cb8b515112d1c9e007feed599b1c1ba
ama <takuyaamano@amamo.local>
2019-05-17 01:52:41 +0900

    [update]finishページ用のルートを設定

--- config/routes.rb
+++ config/routes.rb
@@ -10,16 +10,17 @@ Rails.application.routes.draw do
   resources :users, only: [:show]
 
   # Cart画面は上から順にこのルーティングで進んで行きます。あとから修正している為ちょっと無理矢理感のあるルーティングとなっています。
-  get 'carts/:id/edit',                 to: 'carts#edit', as: 'cart_edit'
-  post 'carts/:id/edit',                to: 'carts#cartedit', as: 'cart_cartedit'
-  get 'carts/:id/ship_to_another/edit', to: 'carts#ship', as: 'ship_to_another_edit'
-  post 'carts/:id/ship_to_another/edit', to: 'carts#shipedit', as: 'ship_to_another_shipedit'
-  get 'carts/:id/payment/edit',         to: 'carts#pay', as: 'payment_edit'
-  post 'carts/:id/payment/edit',         to: 'carts#payedit', as: 'payment_payedit'
+  get 'carts/:id/edit',                 to: 'carts#edit',    as: 'cart_edit'
+  post 'carts/:id/edit',                to: 'carts#cartedit',as: 'cart_cartedit'
+  get 'carts/:id/ship_to_another/edit', to: 'carts#ship',    as: 'ship_to_another_edit'
+  post 'carts/:id/ship_to_another/edit',to: 'carts#shipedit',as: 'ship_to_another_shipedit'
+  get 'carts/:id/payment/edit',         to: 'carts#pay',     as: 'payment_edit'
+  post 'carts/:id/payment/edit',        to: 'carts#payedit', as: 'payment_payedit'
   get 'carts/:id/confirm/edit',         to: 'carts#confirm', as: 'confirm_edit'
+  get 'carts/:id/finish',               to: 'carts#finish',  as: 'finish'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :items do
-    resource :item_cart,  only: [:create, :update]
+    resource :item_cart,  only: [:create, :update, :destroy]
     resource :item_singer, only: [:create]
     resource :review,      only: [:index, :new, :create, :update, :destroy]
   end
--- db/schema.rb
+++ db/schema.rb
@@ -163,17 +163,11 @@ ActiveRecord::Schema.define(version: 2019_05_13_055347) do
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
     t.integer "user_id"
-    t.string "last_name"
-    t.string "first_name"
-    t.string "last_name_kana"
-    t.string "first_name_kana"
-    t.string "postal_code"
-    t.string "addres"
-    t.string "phone_number"
     t.boolean "admin"
     t.datetime "deleted_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
