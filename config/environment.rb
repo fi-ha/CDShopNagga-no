21cbc603c068994898bd0d234c5bb56c27bbf13f
takuyaamamo <iamaama703@gmail.com>
2019-05-21 11:40:35 +0900

    [fix][update]アクションメール設定の記載場所をまちがえたdevelopment.rbにコピペ、コメントアウトの記入

--- config/environment.rb
+++ config/environment.rb
@@ -3,15 +3,3 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-# config.action_mailer.raise_delivery_errors = true
-#   config.action_mailer.delivery_method = :smtp
-#   config.action_mailer.smtp_settings = {
-#     port:                 587,
-#     address:              'smtp.gmail.com',
-#     domain:               'smtp.gmail.com',
-#     user_name:            "ENV['MAIL_ADDRESS_KEY']",
-#     password:             "ENV['MAIL_PASSWORD_KEY']",
-#     authentication:       'login',
-#     enable_starttls_auto: true
-#   }
