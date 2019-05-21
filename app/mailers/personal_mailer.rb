a851d6031fb499b76370e50a03659732c600e30c
takuyaamamo <iamaama703@gmail.com>
2019-05-21 12:43:31 +0900

    [update]メール設定を記入

--- app/mailers/personal_mailer.rb
+++ app/mailers/personal_mailer.rb
@@ -1,7 +1,15 @@
 class PersonalMailer < ApplicationMailer
+ #個別のメール設定
+
+  # 個別のメール設定をdefineで設定していく。それぞれ使いたい部分で関数を呼び出せば使えるんだと思う
+
+  # 下記部分は見本のdefine
   def send_when_create(user)
+    # テンプレートたちと、メーラーの関係は、viewとcontrollerと一緒。要するにこのアクションに対応するメールの中身はviews/send_when_create.html.erb or txt.erb
     @user = user
-    mail to:      user.email,
-    subject: 'こんにちは！新しいユーザーが追加されました'
+    #mailはメソッド、mail toでは無いmailの中のto
+    mail to:      user.email,# 送信先にuserのemailアドレス
+         subject: 'こんにちは！新しいユーザーが追加されました'# 題名設定
   end
+
 end
