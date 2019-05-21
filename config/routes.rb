bd6b28e29cb0f1f27643a77566362e14bbff0763
takuyaamamo <iamaama703@gmail.com>
2019-05-21 15:51:52 +0900

    [update]ルートをきれいにした

--- config/routes.rb
+++ config/routes.rb
@@ -7,12 +7,12 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :singers, only: [:index, :edit, :create, :update, :destroy]
-  resources :favorites
-  resources :users, only: [:show]
-
   resources :singers
+  resources :singers, only: [:index, :edit, :create, :update, :destroy]
 
+  resources :favorites
+
+  resources :users, only: [:show]
   resources :users, only: [:show] do
     resource :favorites, only: [:index]
   end
@@ -31,34 +31,34 @@ Rails.application.routes.draw do
   get  'carts/:id/finish',              to: 'carts#finish',  as: 'finish'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :items do
-    resource :item_carts,   only: [:create, :update, :destroy]
-    resource :item_singers, only: [:create]
-    resources :item_genres,      only: [:create, :update]
-    resource :reviews,      only: [:index, :new, :create, :update, :destroy]
-    resource :favorites,    only: [:create, :destroy]
+    resource :item_carts,  only: [:create, :update, :destroy]
+    resource :item_singers,only: [:create]
+    resources :item_genres,only: [:create, :update]
+    resource :reviews,     only: [:index, :new, :create, :update, :destroy]
+    resource :favorites,   only: [:create, :destroy]
   end
 
-  resources :stocks,            only: [:create, :update]
-  resources :reviews,           only: [:index, :edit]
-  resources :item_singers,      only: [:update]
-  resources :genres,           only: [:index, :new, :edit, :create, :update, :destroy]
-  resources :carts,            only: [:index, :new, :create, :update]
-  resources :ship_to_anothers, only: [:create, :new, :update]
+  resources :stocks,          only: [:create, :update]
+  resources :reviews,         only: [:index, :edit]
+  resources :item_singers,    only: [:update]
+  resources :genres,          only: [:index, :new, :edit, :create, :update, :destroy]
+  resources :carts,           only: [:index, :new, :create, :update]
+  resources :ship_to_anothers,only: [:create, :new, :update]
 
-  resources :labels, only: [:index, :create, :edit, :update, :destroy]
+  resources :labels,   only: [:index, :create, :edit, :update, :destroy]
   resources :contacts, only: [:new, :create]
-  resources :responses, only: [:new, :create]
-  resources :contacts,         only: [:new, :create]
-  resources :responses,        only: [:new, :create]
+  resources :responses,only: [:new, :create]
+  resources :contacts, only: [:new, :create]
+  resources :responses,only: [:new, :create]
 
   resources :songs, only: [:create, :update, :destroy]
 
   namespace :administrator do
-    resources :items, only: [:index, :edit, :update, :destroy]
-    resources :carts, only: [:index, :show]
+    resources :items,   only: [:index, :edit, :update, :destroy]
+    resources :carts,   only: [:index, :show]
     resources :reviews, only: [:index, :edit]
-    resources :users, only: [:index]
-    resources :contacts, only: [:index]
+    resources :users,   only: [:index]
+    resources :contacts,only: [:index]
   end
 
   resources :concepts, only: [:index]
