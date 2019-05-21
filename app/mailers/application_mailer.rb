5346896825382f29cce9a6c04a98c9cd502075d2
takuyaamamo <iamaama703@gmail.com>
2019-05-21 15:02:44 +0900

    [upadte]デフォルトのメール設定を追加

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,9 +2,9 @@ class ApplicationMailer < ActionMailer::Base
 
   # ===========================全体のメール設定====================================
   default from:     'FIHAからのメールだよ',# 	メールの送り主の名前
-          bcc:      "ENV['MAIL_ADDRESS_KEY']",# BCCで同時に送る宛先
-          reply_to: "ENV['MAIL_ADDRESS_KEY']"# 返信した場合の受信先アドレス
-          
+          bcc:      ENV['MAIL_ADDRESS_KEY'],# BCCで同時に送る宛先
+          reply_to: ENV['MAIL_ADDRESS_KEY']# 返信した場合の受信先アドレス
+
   # レイアウトはviews/layoutsのyield部分の大元のファイルを無理やり指定できる
   layout 'mailer'
 
