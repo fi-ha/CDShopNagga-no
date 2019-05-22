d05d751b46709ee783dd9b77198ad62816f759ff
takuyaamamo <iamaama703@gmail.com>
2019-05-22 14:08:05 +0900

    [update][create]返信メールフォームからメールを送信できるように設定。

--- app/mailers/personal_mailer.rb
+++ app/mailers/personal_mailer.rb
@@ -6,7 +6,7 @@ class PersonalMailer < ApplicationMailer
   # 下記部分は見本のdefine
   # 下記アクションを使用したい場合はPersonal.update_when_create(user).deliverと指定する
   def send_when_create(user)
-    # テンプレートたちと、メーラーの関係は、viewとcontrollerと一緒。要するにこのアクションに対応するメールの中身はviews/send_when_create.html.erb or txt.erb
+    # テンプレートたちと、メーラーの関係は、viewとcontrollerと一緒。要するにこのアクションに対応するメールの中身はviews/personal_mailer/send_when_create.html.erb or txt.erb
     @user = user
     #mailはメソッド、mail toでは無いmailの中のto
     mail to:      user.email,# 送信先にuserのemailアドレス
@@ -15,10 +15,20 @@ class PersonalMailer < ApplicationMailer
 
 # 下記メソッドを使用したい場合はPersonal.send_when_contact_to_user(@contact).deliverと指定する
   def send_when_contact_to_user(contact)
+    # 引き渡されるvalueに@を付けることでメール画面に反映させられる用にする
     @contact = contact
-    mail  to:      'iamaama703@gmail.com',
-          from:    'iamaama703@gmail.com',
+    mail  to:      ENV['MAIL_ADDRESS_KEY'],
+          from:    ENV['MAIL_ADDRESS_KEY'],
           subject: 'お問い合わせを受け付けました。'
+  end
+
+  # layout "send_when_respon_to_user"
+  def send_when_respon_to_user(respon)
+    @respon = respon
+    @contact = Contact.find_by(id: respon.contact_id)
+    mail  to:      @contact.email,
+          from:    ENV['MAIL_ADDRESS_KEY'],
+          subject: @respon.subject
   end
 
 end
