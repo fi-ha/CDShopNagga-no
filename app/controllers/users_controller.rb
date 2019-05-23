90da8cda8eab6667f70256d6d558cd32056b8a05
takuyaamamo <iamaama703@gmail.com>
2019-05-23 17:37:26 +0900

    [update]マイページで現在ログインユーザーとのマッチング、管理者のみ閲覧可能を設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 	before_action :authenticate_user!
-	
+	before_action :judgment_user
+
 	def show
 		@user = User.find(params[:id])
 	end
@@ -8,4 +9,10 @@ class UsersController < ApplicationController
   private
     def user_params
     end
+
+		def judgment_user
+			unless current_user.id == params[:id].to_i || current_user.admin == true
+				redirect_to(root_path)
+			end
+		end
 end
