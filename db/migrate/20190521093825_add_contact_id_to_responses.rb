19f4765c8bb08bacabf1efb7fa0a9a5405a63558
takuyaamamo <iamaama703@gmail.com>
2019-05-23 12:38:59 +0900

    [fix]不要なマイグレーションファイルを削除しました。

--- db/migrate/20190521093825_add_contact_id_to_responses.rb
+++
@@ -1,5 +0,0 @@
-class AddContactIdToResponses < ActiveRecord::Migration[5.2]
-  def change
-    add_column :responses, :contact_id, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_21_122248) do
+ActiveRecord::Schema.define(version: 2019_05_21_121557) do
 
   create_table "carts", force: :cascade do |t|
     t.integer "user_id"
@@ -107,16 +107,6 @@ ActiveRecord::Schema.define(version: 2019_05_21_122248) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "responses", force: :cascade do |t|
-    t.string "response_name"
-    t.string "subject"
-    t.text "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "contact_id"
-    t.index ["created_at"], name: "index_responses_on_created_at"
-  end
-
   create_table "reviews", force: :cascade do |t|
     t.integer "item_id"
     t.integer "user_id"
@@ -174,6 +164,13 @@ ActiveRecord::Schema.define(version: 2019_05_21_122248) do
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
     t.integer "user_id"
+    t.string "last_name"
+    t.string "first_name"
+    t.string "last_name_kana"
+    t.string "first_name_kana"
+    t.string "postal_code"
+    t.string "addres"
+    t.string "phone_number"
     t.boolean "admin"
     t.datetime "deleted_at"
     t.datetime "created_at", null: false
