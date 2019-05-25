8c010c5efa30a4d27dc02eb90cb3b7eb9d0acb94
takuyaamamo <iamaama703@gmail.com>
2019-05-25 18:38:13 +0900

    [update]使用できるように更新中

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -11,11 +11,6 @@ class CartsController < ApplicationController
   def edit
     # # Cartの中から現在のログインユーザーかつ、statusがカート状態のカートを取り出す関数定義
     # 下記のdefineを別のファイルに保存する場合そのモデルに対応する.rbに書くとよろしい
-    # def current_cart_id
-    #   current_cart = Cart.where(user_id: current_user.id).where(status: 1)
-    #   current_cart_id = current_cart.id
-    #   return current_cart_id
-    # end
     # # cartのidとアソシエーションしているItem_cartを取り出す
     # item_carts = Item_carts.find(cart_id: current_cart_id)
     # # 更にitem_cartsとアソシエーションしているitemsを取り出す
@@ -62,10 +57,6 @@ class CartsController < ApplicationController
     redirect_to payment_edit_path
   end
 
-  def pay
-    # 特に呼び出すものなし画面を表示しているだけ
-  end
-
   def payedit
     # cart = current_cart_id
     # cart.update(post_params)
@@ -73,35 +64,54 @@ class CartsController < ApplicationController
   end
 
   def confirm
-    # ここはeditアクションと同じ記述だからどこかに逃したい
+    # 現在のカートを取り出す
+    cart = current_user.carts.find_by(status: 1)
     # cartのidとアソシエーションしているItem_cartを取り出す
-    # @item_carts = Item_carts.find(cart_id: current_cart_id)
+    @item_carts = cart.item_carts
     # 更にitem_cartsとアソシエーションしているitemsを取り出す
-    # @items = item_carts.items
+    @items = @item_carts.items
     # itemとアソシエーションしているitem_singers、item_genres、を取り出す
-    # @item_singers = @items.item_singers
-    # @item_genres = @items.item_genres
+    @item_singers = @items.item_singers
+    @item_genres = @items.item_genres
     # Genre,Stocks,Singer,Labelsからアソシエーションで関連しているデータを取り出す。
-    # @genres = @item_genres.Genres
-    # @stocks = @item.Stocks
-    # @singers = @item_singers.Singer
-    # @labels = @items.Labels
+    @genres = @item_genres.genres
+    @labels = @items.labels
     # for で itemsをitemに変換し繰り返すその中で計算合計を出すさらにitem_stockにpriceを保管、sumpriceをcartに保管@sumprice
-    # @ship_to_another = current_cart_id.ship_to_anothers
-    # case cart.payment
-    # when 1 then
-    #   @payment = "<p>銀行振込</p><%= link_to "支払い方法選択に戻る", payment_edit_path %><%= link_to "購入確定", confirm_ginko_path %>"
-    # when 2 then
-    #   @payment = "<p>代金引換</p><%= link_to "支払い方法選択に戻る", payment_edit_path %><%= link_to "購入確定", confirm_daibiki_path %>"
-    # when 3 then
-    #   @payment = "<p>クレジットカード</p><%= link_to "支払い方法選択に戻る", payment_edit_path %><%= link_to "購入確定", confirm_cred_path %>"
-    # else
-    #   @payment = "<p>支払い方法が選択されておりません</p><%= link_to "支払い方法選択に戻る" , payment_edit_path %>"
-    # end
+    @sumprice = 0
+    for item in @items do
+      price = item.price * item.@item_carts.count
+      @sumprice += price
+    end
+
+    cart.total_price = @sumprice
+    cart.save
+
+    if cart.ship_to_another
+      @ship_to_another = cart.ship_to_anothers
+    end
+
+    case cart.payment
+      when 1 then
+       @payment = "<p>銀行振込</p><%= link_to "購入確定", confirm_ginko_path %>"
+
+      when 2 then
+       @payment = "<p>代金引換</p><%= link_to "購入確定", confirm_daibiki_path %>"
+
+      when 3 then
+       @payment = "<p>クレジットカード</p><%= link_to "購入確定", confirm_cred_path %>"
+
+     else
+       @peyment = "<%= link_to "支払い方法選択に戻る" , payment_edit_path %>"
+     end
+
   end
 
   def ginko
-    # 発送メール送信
+    cart = current_user.carts.find_by(status: 1)
+    cart.status = '未発送'
+    cart.total_price = params[:sumprice]
+    cart.save
+    Personal.send_when_ginko_to_user(cart).deliver
     redirect_to finish_path
   end
 
@@ -123,7 +133,6 @@ class CartsController < ApplicationController
   end
 
   private
-
     def post_params
       # ストロングパラメーター、ビューフォームからのコントローラーへの情報受け渡しを以下のカラムのみ許可
         params.require(:cart).permit(:title, :body, :payment, :status)
