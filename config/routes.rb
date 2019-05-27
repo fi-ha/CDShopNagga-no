b3c7b0a5f267017fc4d4cd9c3e7ee7d2965590d3
takuyaamamo <iamaama703@gmail.com>
2019-05-27 15:27:47 +0900

    [update]ユーザーの権限変更を行う設定、これを使用するにはデバイスのオーバーライドが必要

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :singers, only: [:index, :edit, :create, :update, :destroy]
   resources :favorites
-  resources :users, only: [:show]
+  resources :users, only: [:show, :update]
 
   resources :users, only: [:show] do
     resource :favorites, only: [:index]
