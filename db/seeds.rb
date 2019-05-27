2be9f313cb0e6a27f907b3339658d93581062146
takuyaamamo <iamaama703@gmail.com>
2019-05-27 16:58:34 +0900

    [update]消えていたので追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,17 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
+# ============================初期管理者作成======================================
+# User.create!(
+#    email: 'aaa@aaa.aaa',
+#    password: '1111111111',
+#    last_name: '管理者',
+#    first_name: '太郎',
+#    last_name_kana: 'カンリシャ',
+#    first_name_kana: 'タロウ',
+#    postal_code: '1234567',
+#    addres: '東京都渋谷区神南１丁目１９−１１',
+#    phone_number: '08012345678',
+#    admin: 'true',
+# )
+# ==============================================================================
